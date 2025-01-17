748b23298648d4ead752f0f25242519bbd25cb40
Frizz <frizz925@hotmail.com>
2025-01-17 17:08:41 +0900

    feat(main/logging): Use custom msg header

--- audiowire/src/logging.rs
+++ audiowire/src/logging.rs
@@ -1,6 +1,10 @@
-use std::env;
+use std::{
+    env,
+    io::{self, Write},
+};
 
-use slog::{o, Drain, Logger, OwnedKV, SendSyncRefUnwindSafeKV};
+use slog::{o, Drain, Logger, OwnedKV, Record, SendSyncRefUnwindSafeKV};
+use slog_term::{CountingWriter, RecordDecorator, ThreadSafeTimestampFn};
 
 #[inline]
 pub fn term_logger() -> Logger {
@@ -17,7 +21,7 @@ where
     let decorator = slog_term::TermDecorator::new().build();
     let root_builder = slog_term::FullFormat::new(decorator);
     let builder = if timestamp_disabled {
-        root_builder.use_custom_timestamp(|_| Ok(()))
+        root_builder.use_custom_header_print(print_msg_header)
     } else {
         root_builder.use_custom_timestamp(|f| {
             let dt = chrono::Local::now();
@@ -28,4 +32,33 @@ where
     let format_drain = builder.build().fuse();
     let drain = slog_async::Async::new(format_drain).build().fuse();
     slog::Logger::root(drain, kv)
+}
+
+fn print_msg_header(
+    _fn_timestamp: &dyn ThreadSafeTimestampFn<Output = io::Result<()>>,
+    mut rd: &mut dyn RecordDecorator,
+    record: &Record,
+    use_file_location: bool,
+) -> io::Result<bool> {
+    rd.start_level()?;
+    write!(rd, "{}", record.level().as_short_str())?;
+
+    if use_file_location {
+        rd.start_location()?;
+        write!(
+            rd,
+            "[{}:{}:{}]",
+            record.location().file,
+            record.location().line,
+            record.location().column
+        )?;
+    }
+
+    rd.start_whitespace()?;
+    write!(rd, " ")?;
+
+    rd.start_msg()?;
+    let mut count_rd = CountingWriter::new(&mut rd);
+    write!(count_rd, "{}", record.msg())?;
+    Ok(count_rd.count() != 0)
 }
