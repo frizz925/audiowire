9944ccd65dee40e772d7f97ad36b55a22e99aff8
Izra <frizz925@hotmail.com>
2026-02-24 19:46:06 +0900

    feat(log): Remove slog-async to improve performance

--- audiowire/src/logging.rs
+++ audiowire/src/logging.rs
@@ -1,17 +1,31 @@
-use std::{env, str::FromStr};
+use std::{env, io::stderr, str::FromStr};
 
+use chrono::Local;
 use slog::{Drain, Level, o};
 
 pub fn initialize() -> slog::Logger {
     let level = env::var("RUST_LOG")
-        .map_err(|_| ())
-        .and_then(|s| Level::from_str(s.as_str()))
+        .ok()
+        .and_then(|s| Level::from_str(s.as_str()).ok())
         .unwrap_or(Level::Info);
 
-    let decorator = slog_term::TermDecorator::new().build();
-    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let disable_timestamp = env::var("DISABLE_LOG_TIMESTAMP")
+        .map(|s| s == "true")
+        .unwrap_or_default();
+
+    let plain = slog_term::PlainSyncDecorator::new(stderr());
+    let drain = slog_term::FullFormat::new(plain)
+        .use_custom_timestamp(move |f| {
+            if !disable_timestamp {
+                let ts = Local::now().format("%Y-%m-%d %H:%M:%S%.3f").to_string();
+                write!(f, "{ts}")
+            } else {
+                Ok(())
+            }
+        })
+        .build()
+        .fuse();
     let drain = slog::LevelFilter::new(drain, level).fuse();
-    let drain = slog_async::Async::new(drain).build().fuse();
 
     slog::Logger::root(drain, o!())
 }
